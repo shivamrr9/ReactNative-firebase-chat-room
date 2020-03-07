7c7d4cbbbb3671e7b3b047e49e8c110d977c6b61
shivamrr9 <shivamrr9@gmail.com>
2020-03-08 00:54:52 +0530

    react native - firebase public chat room

---
+++ Fire.js
@@ -0,0 +1,86 @@
+import firebase from 'firebase'; // 4.8.1
+
+class Fire {
+  constructor() {
+    this.init();
+    this.observeAuth();
+  }
+
+  init = () => {
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        apiKey: "AIzaSyBWTbq4lt89-nLs4SiqufIpB37tpRORHyM",
+    authDomain: "react-native-chat-72a80.firebaseapp.com",
+    databaseURL: "https://react-native-chat-72a80.firebaseio.com",
+    projectId: "react-native-chat-72a80",
+    storageBucket: "react-native-chat-72a80.appspot.com",
+    messagingSenderId: "364960104599",
+      });
+    }
+  };
+
+  observeAuth = () =>
+    firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
+
+  onAuthStateChanged = user => {
+    if (!user) {
+      try {
+        firebase.auth().signInAnonymously();
+      } catch ({ message }) {
+        alert(message);
+      }
+    }
+  };
+
+  get uid() {
+    return (firebase.auth().currentUser || {}).uid;
+  }
+
+  get ref() {
+    return firebase.database().ref('messages');
+  }
+
+  parse = snapshot => {
+    const { timestamp: numberStamp, text, user } = snapshot.val();
+    const { key: _id } = snapshot;
+    const timestamp = new Date(numberStamp);
+    const message = {
+      _id,
+      timestamp,
+      text,
+      user,
+    };
+    return message;
+  };
+
+  on = callback =>
+    this.ref
+      .limitToLast(20)
+      .on('child_added', snapshot => callback(this.parse(snapshot)));
+
+  get timestamp() {
+    return firebase.database.ServerValue.TIMESTAMP;
+  }
+  // send the message to the Backend
+  send = messages => {
+    for (let i = 0; i < messages.length; i++) {
+      const { text, user } = messages[i];
+      const message = {
+        text,
+        user,
+        timestamp: this.timestamp,
+      };
+      this.append(message);
+    }
+  };
+
+  append = message => this.ref.push(message);
+
+  // close the connection to the Backend
+  off() {
+    this.ref.off();
+  }
+}
+
+Fire.shared = new Fire();
+export default Fire;
